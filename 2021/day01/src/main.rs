77550f1b27e83a95f55f0ec6f311b1151f70fb9f
Mohammad Mahdi Saffari <mmsaffari@gmail.com>
2021-12-05 19:59:16 +0330

    Day 01 is complete.

---
+++ 2021/day01/src/main.rs
@@ -0,0 +1,79 @@
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+
+fn main() {
+    println!("{:?}", String::from_utf8(vec![b'='; 40]));
+    solve_part_i();
+    println!("{:?}", String::from_utf8(vec![b'='; 40]));
+    solve_part_ii();
+    println!("{:?}", String::from_utf8(vec![b'='; 40]));
+}
+
+fn solve_part_i() {
+    let mut increments: i16 = 0;
+    match read_lines("./data/input.txt") {
+        Ok(lines) => {
+            let depts: Vec<i32> = lines
+                .into_iter()
+                .map(|line| line.expect("Can't read this line"))
+                .collect::<Vec<String>>()
+                .into_iter()
+                .map(|d| {
+                    d.parse::<i32>()
+                        .expect("Can't convert a this line to number")
+                })
+                .collect();
+            let fenestres = depts.windows(2);
+            for f in fenestres {
+                if &f[1] > &f[0] {
+                    increments += 1;
+                }
+            }
+        }
+        Err(e) => {
+            println!("ERROR: {}", e);
+        }
+    };
+    println!("PART I - {} increments detected!", increments);
+}
+
+fn solve_part_ii() {
+    let mut increments: i16 = 0;
+    match read_lines("./data/input.txt") {
+        Ok(lines) => {
+            let depts: Vec<i32> = lines
+                .into_iter()
+                .map(|line| line.expect("Can't read this line"))
+                .collect::<Vec<String>>()
+                .into_iter()
+                .map(|d| {
+                    d.parse::<i32>()
+                        .expect("Can't convert a this line to number")
+                })
+                .collect();
+            let sums: Vec<i32> = depts
+                .windows(3)
+                .into_iter()
+                .map(|f| &f[0] + &f[1] + &f[2])
+                .collect();
+            let fenestres = sums.windows(2);
+            for f in fenestres {
+                if &f[1] > &f[0] {
+                    increments += 1;
+                }
+            }
+        }
+        Err(e) => {
+            println!("ERROR: {}", e);
+        }
+    }
+    println!("PART II - {} increments detected!", increments);
+}
+
+fn read_lines(filename: impl AsRef<Path>) -> Result<io::Lines<io::BufReader<File>>, io::Error> {
+    match File::open(filename) {
+        Ok(file) => Ok(io::BufReader::new(file).lines()),
+        Err(e) => Err(e),
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,12 @@
+# Advent of Code
+
+This is my attempt to solve the [Advent Of Code 2021](https://adventofcode.com/2021) puzzles using [Rust](https://www.rust-lang.org/). I'm learning to use Rust, so these attempts may get updated in the future.
+
+The goal is to challenge my old DEV skills, and also learn to use this promissing langauge.
+
+**Comments and improvements are welcome.**
+
+If you would like to run these yourself, save your data in the appropriate `input.txt` file, into the `data` folder of the appropriate `day`, then run `cargo run` inside that day's folder.
+
+- <http://adventofcode.com/2021> :christmas\_tree:
+- <https://www.rust-lang.org/> :gear:
